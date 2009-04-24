0b40225d39e70d9f259c7acf293b4157ad5d94c1
Andy Yates <andy@andy-vmdesktop.(none)>
2009-04-24 16:51:15 -0500

    Made field overridden in the model field classes not required fields.

--- bha/forms.py
+++ bha/forms.py
@@ -38,7 +38,7 @@ class JetForm(ModelForm) :
                 'all': (form_css,)
         }
 
-    value = forms.DecimalField(widget=forms.TextInput(attrs={'size':'6'}) )
+    value = forms.DecimalField(required=False, widget=forms.TextInput(attrs={'size':'6'}) )
 
 class PumpForm(ModelForm) :
     
@@ -50,8 +50,8 @@ class PumpForm(ModelForm) :
                 'all': (form_css,)
         }
     
-    liner_size = forms.DecimalField(widget=forms.TextInput(attrs={'size':'6'}) )
-    stroke_length = forms.DecimalField(widget=forms.TextInput(attrs={'size':'6'}) )
-    stroke_volume = forms.DecimalField(widget=forms.TextInput(attrs={'size':'6'}) )
-    spp_max = forms.DecimalField(widget=forms.TextInput(attrs={'size':'6'}) )
-    efficiency = forms.DecimalField(widget=forms.TextInput(attrs={'size':'6'}) )
+    liner_size = forms.DecimalField(required=False, widget=forms.TextInput(attrs={'size':'6'}) )
+    stroke_length = forms.DecimalField(required=False, widget=forms.TextInput(attrs={'size':'6'}) )
+    stroke_volume = forms.DecimalField(required=False, widget=forms.TextInput(attrs={'size':'6'}) )
+    spp_max = forms.DecimalField(required=False, widget=forms.TextInput(attrs={'size':'6'}) )
+    efficiency = forms.DecimalField(required=False, widget=forms.TextInput(attrs={'size':'6'}) )
